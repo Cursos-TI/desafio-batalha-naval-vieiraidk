0f71c84bb3fbf9c9ce2c1c0400ef9b152df2fa27
vieiraidk <80226915+vieiraidk@users.noreply.github.com>
2025-11-14 18:52:23 -0300

    Nivel avançado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,7 @@
 #define SHIP_SIZE 3
 #define WATER 0
 #define SHIP 3
+#define EFFECT 5
 
 void inicializar_tabuleiro(int board[N][N]);
 bool pode_posicionar_horizontal(int board[N][N], int row, int col_start);
@@ -16,6 +17,13 @@ void posicionar_vertical(int board[N][N], int row_start, int col);
 void posicionar_diagonal_inc(int board[N][N], int row_start, int col_start);
 void posicionar_diagonal_dec(int board[N][N], int row_start, int col_start);
 void imprimir_tabuleiro(int board[N][N]);
+
+/* Habilidades */
+#define SKILL_SIZE 5
+void construir_cone(int skill[SKILL_SIZE][SKILL_SIZE]);
+void construir_cruz(int skill[SKILL_SIZE][SKILL_SIZE]);
+void construir_octaedro(int skill[SKILL_SIZE][SKILL_SIZE]);
+void aplicar_habilidade(int board[N][N], int skill[SKILL_SIZE][SKILL_SIZE], int origin_row, int origin_col);
 
 int main(void) {
     int board[N][N];
@@ -31,6 +39,19 @@ int main(void) {
 
     int d2_row_start = 1;
     int d2_col_start = 9;
+
+    int cone_skill[SKILL_SIZE][SKILL_SIZE];
+    int cruz_skill[SKILL_SIZE][SKILL_SIZE];
+    int octa_skill[SKILL_SIZE][SKILL_SIZE];
+
+    int cone_origin_row = 4;
+    int cone_origin_col = 2;
+
+    int cruz_origin_row = 7;
+    int cruz_origin_col = 7;
+
+    int octa_origin_row = 2;
+    int octa_origin_col = 5;
 
     inicializar_tabuleiro(board);
 
@@ -61,6 +82,14 @@ int main(void) {
         return 1;
     }
     posicionar_diagonal_dec(board, d2_row_start, d2_col_start);
+
+    construir_cone(cone_skill);
+    construir_cruz(cruz_skill);
+    construir_octaedro(octa_skill);
+
+    aplicar_habilidade(board, cone_skill, cone_origin_row, cone_origin_col);
+    aplicar_habilidade(board, cruz_skill, cruz_origin_row, cruz_origin_col);
+    aplicar_habilidade(board, octa_skill, octa_origin_row, octa_origin_col);
 
     imprimir_tabuleiro(board);
 
@@ -137,8 +166,61 @@ void posicionar_diagonal_dec(int board[N][N], int row_start, int col_start) {
     }
 }
 
+void construir_cone(int skill[SKILL_SIZE][SKILL_SIZE]) {
+    int center_col = SKILL_SIZE / 2;
+    for (int r = 0; r < SKILL_SIZE; ++r) {
+        for (int c = 0; c < SKILL_SIZE; ++c) {
+            skill[r][c] = 0;
+        }
+    }
+    int height = 3;
+    for (int r = 0; r < height; ++r) {
+        int left = center_col - r;
+        int right = center_col + r;
+        for (int c = left; c <= right; ++c) {
+            if (c >= 0 && c < SKILL_SIZE) skill[r][c] = 1;
+        }
+    }
+}
+
+void construir_cruz(int skill[SKILL_SIZE][SKILL_SIZE]) {
+    int center = SKILL_SIZE / 2;
+    for (int r = 0; r < SKILL_SIZE; ++r) {
+        for (int c = 0; c < SKILL_SIZE; ++c) {
+            if (r == center || c == center) skill[r][c] = 1;
+            else skill[r][c] = 0;
+        }
+    }
+}
+
+void construir_octaedro(int skill[SKILL_SIZE][SKILL_SIZE]) {
+    int center = SKILL_SIZE / 2;
+    for (int r = 0; r < SKILL_SIZE; ++r) {
+        for (int c = 0; c < SKILL_SIZE; ++c) {
+            int manhattan = (r > center ? r - center : center - r) + (c > center ? c - center : center - c);
+            if (manhattan <= 2) skill[r][c] = 1;
+            else skill[r][c] = 0;
+        }
+    }
+}
+
+void aplicar_habilidade(int board[N][N], int skill[SKILL_SIZE][SKILL_SIZE], int origin_row, int origin_col) {
+    int center = SKILL_SIZE / 2;
+    for (int r = 0; r < SKILL_SIZE; ++r) {
+        for (int c = 0; c < SKILL_SIZE; ++c) {
+            if (skill[r][c] == 1) {
+                int target_row = origin_row + (r - center);
+                int target_col = origin_col + (c - center);
+                if (target_row >= 0 && target_row < N && target_col >= 0 && target_col < N) {
+                    board[target_row][target_col] = EFFECT;
+                }
+            }
+        }
+    }
+}
+
 void imprimir_tabuleiro(int board[N][N]) {
-    printf("Tabuleiro (0 = agua, 3 = navio):\n\n");
+    printf("Tabuleiro (0 = agua, 3 = navio, 5 = area de habilidade):\n\n");
     printf("   ");
     for (int c = 0; c < N; ++c) {
         printf("%2d ", c);
